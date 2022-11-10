81205c8ce4096db44e850bb05e8bc5f61561fc9e
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2022-11-10 03:23:47 -0800

    task 0 space fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
-/**
- * swap - Swap two element
- * @xp: Pointer to element to be swapped
- * @yp: Pointer to element to be swapped
- */
-void swap(int *xp, int *yp)
-{
-    int temp;
-    temp = *xp;
-    *xp = *yp;
-    *yp = temp;
 
-}
 /**
- * bubble_sort - Sorts an array of intergers in ascending order
- * @array: array to be sorted
- * @size: Number of elements in an  array
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    if (size < 2)
-        return;
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
-            }
-        }
-    }
+	size_t i, n, new_n;
+	int swap;
+
+	if (array == NULL || size < 2)
+		return;
+
+	n = size;
+	while (n > 0)
+	{
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
+				print_array(array, size);
+			}
+		}
+		n = new_n;
+	}
 }
